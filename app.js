65dc113d7c68e164817be6a32e0482e467d6430c
jorge <jorge-mor@outlook.es>
2023-08-20 19:24:30 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const crypto = require('node:crypto')
+
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schema/movieSchema')
+
+const app = express()
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ hola: "hola mundo" })
+})
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'https://movies.com',
+            'https://midu.dev'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
+
+app.get('/movies', (req, res) => {
+    //cors se soluciona en el back
+    //metodos normales: GET/HEAD/POST
+    // se puede utilizar un middleware npm i cors -E
+    // if (ACCEPTED_ORIGINS.includes(origin)) {
+    //     res.header('Access-Control-Allow-Origin', origin)
+    //     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+
+    // }
+    // const origin = req.header('origin')
+
+    const { genre } = req.query //en la req esta la query y podemos acceder a ella
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g > g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+//metodos complejos: PUT/PATCH/DELETE UTILIZAN EL CORS PRE-FLIGHT
+//options
+
+app.get('/movies/:id', (req, res) => { //path-to-regex se utiliza por debajo de express
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+    //si no encuentra la movie que devuelva un 404
+    res.status(404).json({ message: 'movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessable Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    // en base de datos
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4
+        ...result.data
+    }
+
+    // Esto no sería REST, porque estamos guardando
+    // el estado de la aplicación en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 3001
+
+app.listen(PORT, () => {
+    console.log(`server successfully listening on port http://localhost:${PORT}`)
+})
+
---
+++ schema/movieSchema.js
@@ -0,0 +1,35 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error: 'Movie title must be a string',
+        required_error: 'Movie title is required'
+    }),
+    year: z.number().int().positive().min(1900).max(2024),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10),
+    poster: z.string().url({
+        message: 'Poster must be an url'
+    }),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Drama', 'Fantasy', 'Sci-Fi', 'Thriller']), {
+        required_error: 'Movie genre required',
+        invalid_type_error: 'Movie genre invalid'
+    }
+    )
+})
+
+function validateMovie(object) {
+    return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object) {
+    return movieSchema.partial().safeParse(object)
+
+}
+
+module.exports = {
+    validateMovie,
+    validatePartialMovie
+}
